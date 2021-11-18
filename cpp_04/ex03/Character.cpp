7f0c8944d9558c5c436d409ef9e8ab17d25ec328
Pyg Argus <mene.nath@protonmail.com>
2021-11-18 14:03:58 +0100

    CPP04-v0.8 : ex03 en cours

--- cpp_04/ex03/Character.cpp
+++ cpp_04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:16:22 by namenega          #+#    #+#             */
-/*   Updated: 2021/11/17 17:48:01 by namenega         ###   ########.fr       */
+/*   Updated: 2021/11/18 14:02:02 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,15 +64,28 @@ std::string const &		Character::getName() const {
 /* ******************************* Functions ******************************** */
 
 void			Character::equip(AMateria* m) {
-
+	if (_count >= 0 && _count < 4) {
+		for (int i = 0; i < 4; i++) {
+			if (this->_inventory[i] == 0) {
+				th-s>_inventory[_count] = m;
+				_count++;
+				return ;
+			}
+		}
+	}
 }
 
 void			Character::unequip(int idx) {
-
+	if (this->_inventory[idx]) {
+		this->inventory[idx] = 0;
+		_count--;
+	}
 }
 
 void			Character::use(int idx, ICharacter& target) {
-	
+	if (this->_inventory[idx] && idx >= 0 && idx < 4) {
+		_inventory[idx]->use(target);
+	}
 }
 
 /* ******************************* Destructor ******************************* */

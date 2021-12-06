a8e3b5b0d1f972d050099b1c7c7a76a318adaf79
namenega <mene.nath@protonmail.com>
2021-12-06 14:33:00 +0100

    CPP07 : done, starting CPP08

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -3,41 +3,84 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
+/*   By: namenega <namenega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:17:32 by namenega          #+#    #+#             */
-/*   Updated: 2021/12/02 16:00:46 by namenega         ###   ########.fr       */
+/*   Updated: 2021/12/06 12:40:56 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
+# include <stdexcept>
+
 template< typename T >
 class Array
 {
 	private:
-		T*		_array;
+		T*				_array;
+		unsigned int	_len;
 	public:
-		Array<T>(void) {
+		Array(void) {
 			this->_array = new T[0];
+			this->_len = 0;
 		};
 
-		Array<T>(unsigned int n) {
-			this->_array = new T[n];
+		Array(unsigned int n) {
+			this->_array = new T[n]();
+			this->_len = n;
 		};
 
-		Array<T>(const T & copy) {
+		Array(const Array & copy) {
+			this->_array = NULL;
 			*this = copy;
 		};
-		~Array<T>() {
+		
+		virtual ~Array() {
 			delete [] this->_array;
 		};
 
-		Array<T> &		operator=(const T & operEqual);
-		Array<T>		operator[]();
+		class ArrayException : public std::exception
+		{
+			virtual const char *what() const throw() {
+				return ("This Element is out of the limits.");
+			};
+		};
 
-		int				size(void) const;
+		Array &		operator=(const Array & operEqual) {
+			if (this != &operEqual) {
+				if (this->_array)
+					delete [] this->_array;
+				this->_array = new T[operEqual._len];
+				this->_len = operEqual._len;
+				for (unsigned int i = 0; i < this->_len; i++)
+					this->_array[i] = operEqual._array[i];
+			}
+			return (*this);
+		};
+
+		T &		operator[](unsigned int const i) {
+			if (i >= this->_len || i < 0)
+				throw ArrayException();
+			else
+				return (this->_array[i]);
+		};
+
+		unsigned int	size(void) const {
+			return (this->_len);
+		};
 };
+
+template<typename T>
+std::ostream &operator<<(std::ostream &os, Array<T> const &other)
+{
+	std::cout << "[";
+	for (size_t i = 0; i < other.size(); i++)
+		os << const_cast<Array<T> &>(other)[i] << ", ";
+	std::cout << "]";
+	return (os);
+}
 
 #endif
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -3,15 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
+/*   By: namenega <namenega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:16:51 by namenega          #+#    #+#             */
-/*   Updated: 2021/12/02 15:17:30 by namenega         ###   ########.fr       */
+/*   Updated: 2021/12/06 12:58:39 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#define MAX_VAL 12
 
-int		main(void) {
-	return (0);
+int main(int, char **)
+{
+	//TEST1
+	std::cout << std::endl;
+	Array<int> empty;
+	std::cout << "\033[1;31mTEST 1\033[0m - Empty array: " << empty << std::endl;
+
+
+	//TEST2
+	Array<int> a(10);
+	std::cout << "\033[1;31mTEST 2\033[0m - Default Array: " << a << std::endl;
+	std::cout << std::endl;
+
+	//TEST3
+	std::cout << "----------" << std::endl;
+	std::cout << std::endl;
+	Array<int> numbers(MAX_VAL);
+	int *mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	std::cout << "\033[1;31mTEST 3\033[0m - Array with unsigned int as param : " << std::endl;
+	std::cout << "Size recieved : \033[1;36m" << MAX_VAL << "\033[0m" << std::endl;
+	std::cout << "Size got: \033[1;36m" << numbers.size() << "\033[0m" << std::endl;
+	std::cout << std::endl;
+
+	//TEST4
+	std::cout << "----------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;31mTEST 4\033[0m - Copy : " << std::endl;
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+
+		std::cout << "array: " << numbers << std::endl;
+		std::cout << "copy: " << test << std::endl;
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	std::cout << "\033[1;34mArray copied sucessfully.\033[0m" << std::endl;
+	std::cout << std::endl;
+
+	//TEST5
+	std::cout << "-----------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;31mTEST 5\033[0m - Trying to access negative index" << std::endl;
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl;
+
+	//TEST6
+	std::cout << "-----------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "\033[1;31mTEST 6\033[0m - Trying to access index >= len" << std::endl;
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl;
+
+	delete[] mirror;
+	return 0;
 }

fb44b963069ca21f30c615df56677954d056b2f6
Pyg Argus <mene.nath@protonmail.com>
2021-12-07 16:29:10 +0100

    CPP08-v0.33 : ex00 done

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namenega <namenega@student.s19.be>         +#+  +:+       +#+        */
+/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 13:21:35 by namenega          #+#    #+#             */
-/*   Updated: 2021/12/06 13:22:27 by namenega         ###   ########.fr       */
+/*   Updated: 2021/12/07 14:22:07 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <algorithm>
+
+class easyfindException : public std::exception
+{
+	public:
+		virtual const char *	what() const throw() {
+			return ("> Occurence cannot be found.");
+		};
+};
+
+template< typename T >
+int &	easyfind(T & i, int j) {
+	typename T::iterator	iter;
+	iter = std::find(i.begin(), i.end(), j);
+	if (iter == i.end())
+		throw easyfindException();
+	return (*iter);
+}
 
 #endif
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -3,15 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namenega <namenega@student.s19.be>         +#+  +:+       +#+        */
+/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 13:22:29 by namenega          #+#    #+#             */
-/*   Updated: 2021/12/06 13:22:48 by namenega         ###   ########.fr       */
+/*   Updated: 2021/12/07 14:24:51 by namenega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <array>
 
-int		main(void) {
+int		main(void)
+{
+	std::cout << "\033[1;31mTEST1\033[0m : Find occurence that exists." << std::endl;
+	try
+	{
+		std::array<int, 5>	ar1 = {0, 1, 2, 3, 4};
+		
+		int finderino = easyfind(ar1, 4);
+		std::cout << "> element found is : \033[1;36m" << finderino << "\033[0m" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
+	std::cout << std::endl;
+	std::cout << "\033[1;31mTEST2\033[0m : Find occurence that does not exist." << std::endl;
+	try
+	{
+		std::array<int, 5>	ar1 = {0, 1, 2, 3, 4};
+		
+		int finderino = easyfind(ar1, 6);
+		std::cout << "> element found is : \033[1;36m" << finderino << "\033[0m" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
